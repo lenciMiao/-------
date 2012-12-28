54fbeb3d5c6e3311f233d84bbdb483396a800628
lenci <lenci@cis-Gateway.local>
2012-12-28 20:49:14 +0800

    Initial Commit

---
+++ Huffman Tree/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Huffman Tree
+//
+//  Created by lenci on 12-12-28.
+//  Copyright (c) 2012年 Soochow University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
